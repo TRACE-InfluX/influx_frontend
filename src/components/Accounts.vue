d866e95bd193996bfba039473c762cf41eb85abe
remytruong <remtruong@live.ca>
2019-04-09 17:08:38 -0700

    added accounts + post feature

--- src/components/Accounts.vue
+++ src/components/Accounts.vue
@@ -3,13 +3,18 @@
         <h1>Accounts yeet</h1>
         <ul>
             <li v-for="account in accounts" :key="account.id">
-                <h1>Name: {{account.name}}</h1>
-
+                <h1>Name: <router-link :to="'/accounts/' + account.id">{{account.name}}</router-link></h1>
                 Email: {{account.email}}<br />
                 Website: {{account.website}}<br />
             </li>
         </ul>
-        <p></p>
+        <form @submit.prevent="send" method="post">
+            <input type="text" v-model="accountform.email" placeholder="Enter email" />
+            <input type="text" v-model="accountform.name" placeholder="Enter name" />
+            <input type="text" v-model="accountform.website" placeholder="Enter website" />
+            <input type="password" v-model="accountform.password" placeholder="Enter password" />
+            <input type="submit" name="Submit" />
+        </form>
     </div>
 </template>
 
@@ -19,7 +24,13 @@
     export default {
         data() {
             return {
-                accounts: []
+                accounts: [],
+                accountform: {
+                    email: '',
+                    password: '',
+                    website: '',
+                    name: ''
+                }
             }
         },
         mounted() {
@@ -28,6 +39,19 @@
                     this.accounts = res.data.data
                 })
                 .catch(alert)
+        },
+        methods: {
+            send: function () {
+                var actualroute = 'http://localhost:4000/v0/accounts';
+
+                const account = {
+                    email: this.accountform.email,
+                    name: this.accountform.name,
+                    website: this.accountform.website,
+                    password: this.accountform.password
+                }
+                API.post(actualroute, { account });
+            }
         }
     }
 </script>
--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 import Router from 'vue-router'
 import Influencers from './components/Influencers'
 import Accounts from './components/Accounts'
+import Account from './components/Account'
 const routes = [
     { path: '/influencers', component: Influencers },
-    { path: '/accounts', component: Accounts}
+    { path: '/accounts/:id', component: Account },
+    { path: '/accounts', component: Accounts }
 ]
 
 export default new Router({routes})
