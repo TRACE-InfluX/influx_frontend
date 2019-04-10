2761eebc0f10f7cd5ca48193768d846282b5b13c
Carlo Mendoza <carlomendoza89@gmail.com>
2019-04-10 16:17:18 -0700

    fixed errors in influencers

--- src/components/influencers/index.vue
+++ src/components/influencers/index.vue
@@ -15,22 +15,16 @@
     <h1> Add New Influencer </h1>
     <form @submit.prevent="addRecord()" method="post">
 
-    <p>Name: <input type="text" v-model="name" required></p>
-    <p>Description: <input type="text" v-model="description" required></p>
-    <p>Activity: <input type="text" v-model="activity" required></p>
-    <p>Relevance: <input type="text" v-model="relevance" required></p>
-    <p>Engagement: <input type="text" v-model="engagement" required></p>
+      <p>Name: <input type="text" v-model="new_influencer.name" required></p>
+      <p>Description: <input type="text" v-model="new_influencer.description" required></p>
+      <p>Activity: <input type="text" v-model="new_influencer.activity" required></p>
+      <p>Relevance: <input type="text" v-model="new_influencer.relevance" required></p>
+      <p>Engagement: <input type="text" v-model="new_influencer.engagement" required></p>
 
-    <input type="submit" value='Add' @click='addRecord(influencer.id)'>
-    <input type="reset" value="Clear" @click='resetRecord();'>
+      <input type="submit" value='Add' />
+      <input type="reset" value="Clear" @click='resetRecord' />
     </form>
-
-
-
   </ul>
-
-
-
 </template>
 
 
@@ -41,7 +35,8 @@ import API from 'axios'
 export default {
   data() {
     return {
-      influencers: []
+      influencers: [],
+      new_influencer: {}
     }
   },
   mounted() {
@@ -62,36 +57,17 @@ export default {
     deleteRecord(id){
       API.delete('http://localhost:4000/v0/influencers/'+id)
       .then(this.loadInfluencers)
-    API.get('http://localhost:4000/v0/influencers')
-      .then(res => {
-        this.influencers = res.data.data
-          .sort((a,b)=>{
-            return (b.activity + b.relevance + b.engagement)/3
-                  -(a.activity + a.relevance + a.engagement)/3
-          })
-      })
       .catch(alert)
     },
-    resetRecord(form){
-      var inputs = form.getElementsByTagName('input');
-    for (var i = 0; i<inputs.length; i++) {
-        switch (inputs[i].type) {
-            // case 'hidden':
-            case 'text':
-                inputs[i].value = '';
-                break;
-        }
-    }
+    resetRecord(){
+      this.new_influencer = {}
     },
-    addRecord: function(){
-      const influencer = {
-      name: this.name,
-      description: this.description,
-      activity: this.activity ,
-      relevance: this.relevance,
-      engagement: this.engagement,
-      }
-      API.post('http://localhost:4000/v0/influencers', {influencer});
+    addRecord(){
+      const influencer = this.new_influencer
+      API.post('http://localhost:4000/v0/influencers', {influencer})
+        .then(this.resetRecord)
+        .then(this.loadInfluencers)
+        .catch(alert)
     }
   }
 }
