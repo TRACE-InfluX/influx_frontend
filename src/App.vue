84fa50ed7ad3209b5457ee2ccfe0be7d82c9dd47
avong4 <avong4@my.bcit.ca>
2019-04-10 20:23:03 -0700

    shiny

--- src/App.vue
+++ src/App.vue
@@ -22,11 +22,31 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 
-  #welcome {
-    margin: auto;
-    width: 50%;
-    text-align: center;
-  }
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
 </style>
