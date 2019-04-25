70fe2c3fccb62875fc2fb166cc729f810f5b9739
Carlo Mendoza <carlomendoza89@gmail.com>
2019-04-25 14:21:43 -0700

    css edits

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,32 +1,14 @@
 <!-- template for a login page.-->
 <template>
-<form class="login-form" @submit.prevent="submit">
-    <h2>Login</h2>
-    <input type="text"  v-model="input.username" required placeholder="Email">
-    <input type="password"  v-model="input.password" required placeholder="Password">
-    <button type="button" v-on:click="login()">Login</button>
-    <router-link to="">Forgot Password</router-link>
-</form>
-<!--=======-->
+    <form class="login-form" @submit.prevent="submit">
+        <h2>Login</h2>
+        <img src="@/assets/logo.png">
+        <input type="text"  v-model="input.username" required placeholder="Email">
+        <input type="password"  v-model="input.password" required placeholder="Password">
+        <button type="button" v-on:click="login()">Login</button>
 
-<!--<div class="login">-->
-     <!--<h2>Login</h2>-->
-    <!---->
-    <!--<form id="form"-->
-          <!--@submit.prevent="submit">-->
-
- <!--<div class="user-profile"></div>      -->
- <!--<p><input type="text"  v-model="input.username" required placeholder="Email"></p>-->
- <!--<p><input type="password"  v-model="input.password" required placeholder="Password"></p>-->
-
-
- <!--<button type="button" v-on:click="login()">Login</button>                   -->
-
-     <!---->
-        <!--<p>Forgot your login details?</p><router-link to="">Get help signing in.</router-link></p>-->
-    <!--</form>-->
-<!--</div>-->
-<!--&gt;>>>>>> fc8811d31e85540603acbf0083908b2b2043ec08-->
+        <p>Forgot your login details?</p><p><router-link to="">Get help signing in.</router-link></p>
+    </form>
 </template>
 
 <script>
@@ -56,62 +38,53 @@
     }
 </script>
 
-<style lang="scss" scoped>
-    .login-form > *{
-        display: block;
-        margin: 10px auto;
-    }
-/*<style scoped>*/
-
-/*@import url('https://fonts.googleapis.com/css?family=Lato');*/
+<!--<style lang="scss" scoped>-->
+    <!--.login-form > *{-->
+        <!--display: block;-->
+        <!--margin: 10px auto;-->
+    <!--}-->
+<style scoped>
 
 
-/*.login {*/
-    /*width: 100%;*/
-    /*height: 100%;*/
-    /*font-family: Lato; */
-/*}*/
+img {
+    display: inline-block;
+}
 
-/*img {*/
-    /*display: inline-block;*/
-/*}*/
+.login-form {
+    padding: 1em;
+    margin: auto;
+    display: block;
+    width: 240px;
+}
 
-/*#form {*/
-    /*width: 50%;*/
-    /*padding: 1em;*/
-    /*display: inline-block;*/
-/*}*/
+.login-form > * {
+    display: block;
+    width: 100%;
+    margin: 16px auto;
+}
 
-/*input {*/
-    /*font-family: Lato; */
-    /*width: 200px; */
-    /*height: 30px; */
-    /*display: inline-block;*/
-/*}*/
+input {
+    height: 30px;
+    padding-left: 8px;
+}
 
-/*button { */
-    /*width: 200px;*/
-    /*height: 40px; */
-    /*background-color: #458eff;*/
-    /*color: white; */
-    /*padding: 10px; */
-    /*border-radius: .5em; */
-    /*font-weight: 800; */
-    /*font-family: Lato; */
-    /*display: inline-block;*/
-    /**/
-/*}*/
+button {
+    height: 40px;
+    background-color: #458eff;
+    color: white;
+    padding: 10px;
+    border-radius: .5em;
+    font-weight: 800;
+    display: inline-block;
+}
 
-/*.user-profile {*/
+.login-form img {
+  height: 150px;
+  width: 150px;
+  background-color: #bbb; /* backup background */
+  border-radius: 150px;
+  display: block;
 
-  /*background-image:url('../assets/logo.png');*/
-  /*background-position: center center;*/
-  /*height: 150px;*/
-  /*width: 150px;*/
-  /*background-color: #bbb; !* backup background *!*/
-  /*border-radius: 50%;*/
-  /*display: inline-block;*/
-
-/*}*/
+}
 
 </style>
