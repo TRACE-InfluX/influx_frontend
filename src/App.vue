20726995c1db9ff6db8bd26736bf64eddbc76c0f
suddenly-eva <suddenlyeva@gmail.com>
2019-04-29 15:52:40 -0700

    homepage header and merge develop

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,95 @@
 <!--home view default view with router view for rendering sub components-->
 <template>
-    <div id="app">
-        <nav>
-            <router-link to="/"></router-link>
-            <div class="profile">
-                <router-link to="/register">Register</router-link>
-                <router-link to="/login">Login</router-link>
-                <img src="@/assets/profile.png">
-            </div>
-        </nav>
-        <router-view></router-view>
-        <router-link to="/influencers">Influencers</router-link> &nbsp;
-        <router-link to="/accounts">Accounts</router-link>
-    </div>
+  <div id="app">
+
+    <nav>
+      <router-link to="/" tag="h1">InfluX</router-link>
+      <router-link to="/register">Register</router-link>
+      <router-link to="/login">Login</router-link>
+      <router-link class="profile" to="/accounts/me" />
+    </nav>
+
+    <router-view></router-view>
+
+  </div>
 </template>
+
 <script>
-    export default {
-        name: 'app',
-        mounted() {
-          this.$router.push('/')
-        }
+  export default {
+    name: 'app',
+    mounted() {
+      this.$router.push('/')
     }
+  }
 </script>
 
 <!--styling for top level of app-->
-<style>
+<style lang="scss">
 
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
+  $header-height: 8 * $units;
 
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    > nav {
+      display: flex;
+      justify-content: flex-end;
+      position: fixed;
+      height: $header-height;
+      color: white;
+      background-color: rgba(0,0,0,0.5);
+      width: 100%;
+
+      > * {
+        display: inline-block;
+        line-height: $header-height;
+      }
+
+      h1 {
+        margin-right: auto;
+        font-family: 'Puritan', sans-serif;
+        font-size: 4 * $units;
+        padding-left: 3 * $units;
+        letter-spacing: 1.5 * $units;
+        cursor: pointer;
+        transition: 0.2s ease;
+
+        &:hover {
+          transition: 0.2s ease;
+          letter-spacing: 1.8 * $units;
+        }
+      }
+
+      a {
+        width: 15 * $units;
+        font-size: 2.5 * $units;
+        text-decoration: none;
         text-align: center;
-        color: #2c3e50;
-    }
 
-    nav {
-        text-align: left;
-        height: 100px;
-    }
+        &:visited {
+          color: white;
+        }
 
-    nav > a {
-        background-image: url("assets/influxlogo.png");
-        background-repeat: no-repeat;
-        display: inline-block;
-        width: 250px;
-        height: 100px;
-    }
+        &:hover {
+          text-decoration: underline;
+        }
+      }
 
-    .profile {
-        float: right;
-        display: flex;
-        width: 250px;
-    }
+      .profile {
+        width: $header-height;
+        background: url('~@/assets/profile.png');
+        background-size: 100%;
+      }
 
-    .profile > img {
-        height: 100px;
     }
+  }
 
-    .profile > a {
-        flex: 1;
-        height: 100px;
-        line-height: 100px;
-    }
-
-    h1, h2 {
-        font-weight: normal;
-    }
-
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-
-    a {
-        color: #42b983;
-    }
 </style>
