1585dd8f302f0ce4232237522d35ef304051d5d3
Carlo Mendoza <carlomendoza89@gmail.com>
2019-04-29 14:40:33 -0700

    added popular and made edits to influencer view

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,16 +1,64 @@
 <template>
-    <header class="home-page">
-        <form>
-            <h1>Connect With Your World</h1>
-            <h2>Discover Influencers Today!</h2>
-            <input type="text" placeholder="Search">
-            <button type="button">Discover</button>
-        </form>
-    </header>
+    <main>
+        <header class="home-page">
+            <form>
+                <h1>Connect With Your World</h1>
+                <h2>Discover Influencers Today!</h2>
+                <input type="text" placeholder="Search">
+                <button type="button">Discover</button>
+            </form>
+        </header>
+        <div class="popular">
+            <h1>Popular</h1>
+                <li v-for="popularInfluencer in popular" :key="popularInfluencer.id">
+                    <div class="tile" @click="open(popularInfluencer.id)">
+                        <img src="@/assets/profile.png">
+                        <influencer-view :influencer="popularInfluencer" type="tile"/>
+                    </div>
+                </li>
+        </div>
+        <dialog open="!dialog">
+            <influencer-view :influencer="popular[selected_influencer]" type="detailed"/>
+        </dialog>
+    </main>
 </template>
 
 <script>
+    import InfluencerView from "./influencers/InfluencerView";
     export default {
+        components: {InfluencerView},
+        props: [
+            // dialog = false;
+        ],
+        data() {
+            return {
+                popular: [
+                    {
+                        "id": 0,
+                        "name": "Subtle Asian Cat",
+                        "description": "A surprised Pikachu",
+                        "activity": 111,
+                        "relevance": 222,
+                        "engagement": 333
+                    },
+                    {
+                        "id": 1,
+                        "name": "Dankiel Yeetington",
+                        "description": "Dankest yeet watch maker",
+                        "activity": 44,
+                        "relevance": 55,
+                        "engagement": 66
+                    }
+                ],
+                selected_influencer: '',
+            }
+        },
+        methods: {
+            open(id) {
+                this.selected_influencer = id;
+                this.dialog = !this.dialog;
+            }
+        }
     }
 </script>
 
@@ -45,5 +93,26 @@
         background-color: #458eff;
         color: white;
         border: 0;
+    }
+
+    .popular {
+        height: 200px;
+        display: inline-block;
+        padding: 12px;
+    }
+    .popular img {
+        height: 100px;
+        width: 100px;
+    }
+
+    .tile {
+        border: 1px solid grey;
+        padding: 12px;
+    }
+
+    dialog {
+        border: 1px solid grey;
+        padding: 12px;
+        margin: auto;
     }
 </style>
