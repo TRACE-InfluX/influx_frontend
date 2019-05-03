72650bc0ca1aff787d7f3e04aad8191e09be3186
Aaron Vong <avong4@my.bcit.ca>
2019-05-03 03:17:33 +0100

    Formatted detailed closer to the figma prototype

--- src/components/influencers/InfluencerView.vue
+++ src/components/influencers/InfluencerView.vue
@@ -1,19 +1,38 @@
 <!--Template used for rendeing an individual influencer - used for show / GET influencer by ID-->
 <template>
-
+    <!-- added the entire influencer listing inside of the influencer-view to create an expanded thing, janky af -->
     <article v-if="type === 'detailed'" class="influencer-view">
-        <h2>{{influencer.name}}</h2>
-        <h3>{{influencer.description}}</h3>
-        <p>Activity: {{influencer.activity}}</p>
-        <p>Relevance: {{influencer.relevance}}</p>
-        <p>Engagement: {{influencer.engagement}}</p>
-        <button>Contact</button>
-        <button>Favourite</button>
-        <p>Reach</p>
-        <p>Conversion</p>
-        <p>Cost</p>
-        <p>Areas of Influence</p>
-        <p>Word Cloud</p>
+        <div class ="col-left">
+          <img src="~@/assets/rose.png">
+        </div> <!-- col-left= -->
+        <div class = "col-right">
+          <h2>{{influencer.name}}</h2>
+          <p>{{influencer.description}}</p>
+          <p>{{influencer.url}}</p>
+          <p>Following: {{influencer.following}} | Following: {{influencer.following}} | Posts: {{influencer.posts}}</p>
+
+          <hr> <!-- I'll replace this with an actual divider -->
+          <br> <!--probably want a div or something instead of just using br to add space -->
+          <p>Insert a bar with Activity here</p>
+          <p>Insert a bar with Relevance here</p>
+          <p>Insert a bar with Engagement here</p>
+        </div> <!-- col-right" -->
+        <hr> <!-- I'll replace this with an actual divider -->
+        <div class = "col-left-details">
+          <!-- Add the snapshot of socials here -->
+        </div> <!-- col-left-details"-->
+        <div class = "col-right-details">
+          <button>Contact</button>
+          <button class ="inline"> Favourite </button>
+          <p>Activity: {{influencer.activity}}</p>
+          <p>Relevance: {{influencer.relevance}}</p>
+          <p>Engagement: {{influencer.engagement}}</p>
+          <p>Reach: {{influencer.reach}}</p>
+          <p>Conversion: {{influencer.conversion}}</p>
+          <p>Cost: {{influencer.cost}}</p>
+          <p>Areas of Influence: {{influencer.location}}</p>
+          <p>Word Cloud</p>
+        </div> <!-- col-right-details -->
     </article>
 
     <article v-else-if="type === 'tile'" class="influencer-view influencer-tile">
@@ -30,6 +49,10 @@
           <p>{{influencer.url}}</p>
           <p>Following: {{influencer.following}} | Following: {{influencer.following}} | Posts: {{influencer.posts}}</p>
           <hr>
+          <br> <!--probably want a div or something instead of just using br to add space -->
+          <p>Insert a bar with Activity here</p>
+          <p>Insert a bar with Relevance here</p>
+          <p>Insert a bar with Engagement here</p>
       </div> <!-- col-right" -->
     </article>
 
@@ -95,6 +118,74 @@
     }
 
   
+  }
+
+  .influencer-view{
+
+    border: 1px solid #999999;
+    box-sizing: border-box;
+    border-radius: 2 * $units;
+    height: 80 * $units;
+
+
+      .col-right-details{
+      float:right;
+      width:60%;
+      text-align:left !important;
+      margin-left:10% !important;
+
+      //gap between buttons
+  
+      .inline{ 
+        margin: 3 * $units;
+      }
+
+      button {
+      height: 5 * $units;
+      background-color: #FFFFFF;
+      color: #458eff;
+      display: inline-block;
+      width:25 * $units;
+      border: solid #99999999;
+      border-width: 1px;
+      border-radius: 10px;
+    }
+
+      }
+      .col-right{
+      float:right;
+      width:66.6%;
+      padding-top:2%;
+      padding-bottom:2%;
+      height: 30 * $units;
+      padding-right:10%;
+      padding-left:10%;
+
+      p {
+        color: #999999;
+      }
+
+    }
+
+    .col-left{
+      float:left;
+      width:33.3%;
+      height: 30 * $units;
+
+      img{
+        padding-top: 5%;
+        padding-bottom: 5%;
+        max-width: 30 * $units;
+        max-height: 30 * $units;
+        border-radius:50%;
+      }
+
+    }
+
+
+    h2{
+      font-size: 3 * $units;
+    }
   }
 
   .influencer-listing{
