cd1cc11f9131df19fe78ede9a9651d7bcbc5bb50
suddenly-eva <suddenlyeva@gmail.com>
2019-05-07 20:59:35 -0700

    extract into functions to set influencers in one commit, sort the result set by relevance

--- src/store.js
+++ src/store.js
@@ -11,18 +11,71 @@ function decode_weights(influencers) {
   return influencers
 }
 
+function average_weight(influencers) {
+
+  let accumulator = (pool, next) => {
+    for(let key of Object.keys(next.weights)) {
+      pool[key] = next.weights[key] + (pool[key] || 0)
+    }
+    return pool
+  }
+
+  let avg = influencers.reduce(accumulator,{})
+
+  for (let key of Object.keys(avg)) {
+    avg[key] = (avg[key] - 1) / influencers.length
+  }
+
+  return avg
+}
+
+function normalize(average_weights, influencers) {
+
+  for(let influencer of influencers) {
+
+    let new_weights = {}
+
+    for(let key of Object.keys(influencer.weights)) {
+      let avg = average_weights[key]
+      if (avg) {
+        new_weights[key] = (influencer.weights[key] - avg) / (avg + 1)
+        if (key[0] == '#') {
+          new_weights[key] *= 2
+        }
+      }
+    }
+    influencer.weights = new_weights
+  }
+
+  return influencers
+
+}
+
+function calculate_relevance(influencers) {
+  let account_weights = influencers.find(i => i.username == '@serotoninplus').weights
+
+  for(let influencer of influencers) {
+    let union = {...account_weights, ...influencer.weights}
+    let square_sum = 0
+    for(let key of Object.keys(union)) {
+      let distance = (account_weights[key] || 0) - (influencer.weights[key] || 0)
+      square_sum += distance * distance
+    }
+    influencer.relevance = Math.sqrt(square_sum)
+  }
+
+  return influencers
+}
+
 let state =  {
   influencers: [],
-  popular: [{},{},{},{}],
-  weight_average: {}
+  popular: [{},{},{},{}]
 }
 
 let actions = {
   async load_influencers({commit}, page = 0 ) {
     let res = await API.get('/v0/influencers', { page })
     commit('set_influencers', res.data)
-    commit('generate_weight_average')
-    commit('normalize_weights')
   },
   async load_popular({commit}) {
     let res = await API.get('/v0/influencers/popular')
@@ -32,47 +85,15 @@ let actions = {
 
 let mutations = {
   set_influencers(state, influencers) {
-    state.influencers = decode_weights(influencers)
+    let decoded = decode_weights(influencers)
+    let average_weights = average_weight(decoded)
+    let normalized = normalize(average_weights, decoded)
+    let processed_influencers = calculate_relevance(normalized)
+    processed_influencers.sort((a,b) => b.relevance - a.relevance)
+    state.influencers = processed_influencers
   },
   set_popular(state, popular) {
     state.popular = decode_weights(popular)
-  },
-  generate_weight_average(state) {
-
-    let accumulator = (pool, next) => {
-      for(let key of Object.keys(next.weights)) {
-        pool[key] = next.weights[key] + (pool[key] || 0)
-      }
-      return pool
-    }
-
-    let avg = state.influencers.reduce(accumulator,{})
-
-    for (let key of Object.keys(avg)) {
-      avg[key] = (avg[key] - 1) / state.influencers.length
-    }
-    state.weight_average = avg
-  },
-  normalize_weights(state) {
-
-    let new_influencers = JSON.parse(JSON.stringify(state.influencers))
-
-    for(let influencer of new_influencers) {
-
-      let new_weights = {}
-
-      for(let key of Object.keys(influencer.weights)) {
-        let avg = state.weight_average[key]
-        if (avg) {
-          new_weights[key] = (influencer.weights[key] - avg) / (avg + 1)
-          if (key[0] == '#') {
-            new_weights[key] *= 5
-          }
-        }
-      }
-      influencer.weights = new_weights
-    }
-    state.influencers = new_influencers
   }
 }
 
