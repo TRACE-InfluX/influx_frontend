106bbb8f05ea44835638d6ed2abb18bdff7e3532
suddenly-eva <suddenlyeva@gmail.com>
2019-05-07 19:56:18 -0700

    calculate weight average and normalize weight files to be a proportional difference from the average.

--- src/store.js
+++ src/store.js
@@ -13,7 +13,21 @@ function decode_weights(influencers) {
 
 let state =  {
   influencers: [],
-  popular: [{},{},{},{}]
+  popular: [{},{},{},{}],
+  weight_average: {}
+}
+
+let actions = {
+  async load_influencers({commit}, page = 0 ) {
+    let res = await API.get('/v0/influencers', { page })
+    commit('set_influencers', res.data)
+    commit('generate_weight_average')
+    commit('normalize_weights')
+  },
+  async load_popular({commit}) {
+    let res = await API.get('/v0/influencers/popular')
+    commit('set_popular', res.data)
+  }
 }
 
 let mutations = {
@@ -22,17 +36,43 @@ let mutations = {
   },
   set_popular(state, popular) {
     state.popular = decode_weights(popular)
-  }
-}
-
-let actions = {
-  async load_influencers({commit}, page = 0 ) {
-    let res = await API.get('/v0/influencers', { page })
-    commit('set_influencers', res.data)
   },
-  async load_popular({commit}) {
-    let res = await API.get('/v0/influencers/popular')
-    commit('set_popular', res.data)
+  generate_weight_average(state) {
+
+    let accumulator = (pool, next) => {
+      for(let key of Object.keys(next.weights)) {
+        pool[key] = next.weights[key] + (pool[key] || 0)
+      }
+      return pool
+    }
+
+    let avg = state.influencers.reduce(accumulator,{})
+
+    for (let key of Object.keys(avg)) {
+      avg[key] = (avg[key] - 1) / state.influencers.length
+    }
+    state.weight_average = avg
+  },
+  normalize_weights(state) {
+
+    let new_influencers = JSON.parse(JSON.stringify(state.influencers))
+
+    for(let influencer of new_influencers) {
+
+      let new_weights = {}
+
+      for(let key of Object.keys(influencer.weights)) {
+        let avg = state.weight_average[key]
+        if (avg) {
+          new_weights[key] = (influencer.weights[key] - avg) / (avg + 1)
+          if (key[0] == '#') {
+            new_weights[key] *= 5
+          }
+        }
+      }
+      influencer.weights = new_weights
+    }
+    state.influencers = new_influencers
   }
 }
 
