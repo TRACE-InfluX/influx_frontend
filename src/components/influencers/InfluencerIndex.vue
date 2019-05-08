2b02b9db734813b6985b863b8d779704f7ea6340
suddenly-eva <suddenlyeva@gmail.com>
2019-05-07 18:26:36 -0700

    improve rendering behaviour. query matching now requires a match on every keyword.

--- src/components/influencers/InfluencerIndex.vue
+++ src/components/influencers/InfluencerIndex.vue
@@ -5,15 +5,16 @@
     <ul>
       <h2>{{ filtered_influencers.length }} results</h2>
       <!--Looping through all influencers retrieved from API call to backend-->
-      <li v-for="influencer in rendered_influencers" :key="influencer.id">
+      <li v-for="influencer in rendered_influencers" 
+        :key="influencer.id"
+        @mouseover="render_more(influencer)"
+      >
         <influencer-view 
           :influencer="influencer"
           :type="getType(influencer.id)"
           @click.native="select(influencer.id)"
-          @mouseover.native="update_limit(influencer)"
         />
       </li>
-      <br>
     </ul>
   </div> <!--influencer index -->
 </template>
@@ -28,7 +29,7 @@ export default {
       return {
         selected_influencer: '',
         query: '',
-        limit: 5
+        render_limit: 4
     }
   },
   mounted() {
@@ -42,33 +43,43 @@ export default {
   computed: {
     ...STATE,
     keywords() {
-      return this.query.split(' ')
+      return this.query.toLowerCase().split(' ')
     },
     filtered_influencers() {
-      // Return full list if no query or no list loaded
-      return (!this.query ? this.influencers:
-        // Else check for match between keywords and any part of name/description
-        this.influencers.filter(influencer => {
-          return this.keywords.find(
-            k => {
-              let key = k.toLowerCase()
-              return influencer.name.toLowerCase().includes(key) ||
-                     influencer.description.toLowerCase().includes(key) ||
-                     influencer.weights[key] ||
-                     influencer.weights['#' + key] ||
-                     influencer.location.toLowerCase().includes(key)
-            }
-          )
-        })
-      )
-      .sort((a,b) => b.engagement - a.engagement)
+      // Return full list if no query
+      return !this.query ? this.influencers:
+      // Else filter by keywords
+      this.influencers.filter(influencer => {
+        for (let key of this.keywords) {
+          // Test that every key matches at least one criterion
+          if ( !influencer.name.toLowerCase().includes(key)
+            && !influencer.username.toLowerCase().includes(key)
+            && !influencer.description.toLowerCase().includes(key)
+            && !influencer.location.toLowerCase().includes(key)
+            && !influencer.weights[key]
+            && !influencer.weights['#' + key]
+          ) {
+            // Reject the influencer if any key matches zero criteria
+            return false
+          }
+        }
+        // If all keys have a match, allow through the filter
+        return true
+      })
+    },
+    sorted_influencers() {
+      // Currently the API returns results sorted by engagement already
+      // We will eventually need to improve this function
+      return this.filtered_influencers
+      //.sort((a,b) => b.engagement - a.engagement)
     },
     rendered_influencers() {
-      return this.filtered_influencers.slice(0,this.limit)
+      return this.sorted_influencers.slice(0,this.render_limit)
     }
   },
   watch: {
     query(q) {
+      this.render_limit = 4
       localStorage.setItem("query", q)
       scrollTo(0,0)
     }
@@ -80,8 +91,10 @@ export default {
     getType(id) {
       return id === this.selected_influencer ? "detailed" : "listing"
     },
-    update_limit(influencer) {
-      this.limit = 5 + this.filtered_influencers.indexOf(influencer)
+    render_more(influencer) {
+      // This will lazy load profiles into the ui to make the experience smoother
+      // it will always render a depth 10 more than the current hover
+      this.render_limit = Math.max(this.render_limit, 10 + this.filtered_influencers.indexOf(influencer))
     }
   }
 }
@@ -89,39 +102,38 @@ export default {
 
 <style lang="scss" scoped>
 
+  ul {
+    width: 100%;
+  }
+
   li {
 
     display: block;
-    margin:auto;
+    width: 100%;
     padding: 2 * $units;
-    display:inline;
-
-    
 
   }
   .influencer-index {
   
     padding-top: 10%;
-    padding-left:5%;
-    padding-right:5%;
     text-align: center;
     
   }
 
   .search-bar {
-        $height: 5 * $units;
-        font-size: 1.8 * $units;
-        width: 57 * $units; 
-        height: $height;  
-        border-radius: 0.5 * $units;
-        border:none;
-        box-shadow: $shadow;
-        padding-left: 1 * $units;
-        padding-right:1 * $units;
-        margin-right: 1 * $units;
-        padding-right: 1 * $units;
-        margin-bottom: 5%;
-        margin-top: 5%;
-      }
+    $height: 5 * $units;
+    font-size: 1.8 * $units;
+    width: 57 * $units; 
+    height: $height;  
+    border-radius: 0.5 * $units;
+    border:none;
+    box-shadow: $shadow;
+    padding-left: 1 * $units;
+    padding-right:1 * $units;
+    margin-right: 1 * $units;
+    padding-right: 1 * $units;
+    margin-bottom: 5%;
+    margin-top: 5%;
+  }
   
 </style>
