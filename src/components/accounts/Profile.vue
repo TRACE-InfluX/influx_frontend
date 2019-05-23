23783949ff5d78d41494f1b3ca66dd53932e5d4c
tonihe <tonihe7@gmail.com>
2019-05-22 23:03:57 -0700

    profile css makeover

--- src/components/accounts/Profile.vue
+++ src/components/accounts/Profile.vue
@@ -5,9 +5,9 @@
     <body>
 
 
+  
+    
     <div class="container">
-    
-    
     <div class="nav">    
 
     <div class="menu">
@@ -20,6 +20,8 @@
         </ul>    
 
     </div>
+    </div>
+
 
      <div class="dash-content">
     <h3 class="welcomemsg">Welcome to Influx!</h3>    
@@ -30,7 +32,7 @@
      
     <div class="logout"><button class="buttonbar" @click="logout">Logout</button></div>
     </div>
-    </div>
+    
     <!-- end of dash-content --> 
 
   
@@ -98,131 +100,27 @@ methods: {
     box-sizing: border-box;
   }
 
-  header {
-    
-      margin-top: 15 * $units; 
+ .container {
+     margin-top: 40 * $units; 
+     display: grid; 
+     grid-template-areas: 'navigation dash
+                          dashcontent auto'; 
+ }
+
+
+  .nav 
+  {
+      grid-area: navigation; 
   }
 
-  body {
-        
-    font-family: 'Puritan', sans-serif;
-    line-height: 1.4;
+  .menu 
+  {
+      grid-area: dash; 
   }
 
-    .nav {
-        display: inline-block; 
-        height: 60vh; 
-        width: 20vw; 
-        padding: 5 * $units; 
-        border-radius: 2%;
-        border: 1px solid var(--bordercol);
-        margin-left: 25 * $units;   
-        margin-top: 10 * $units;  
-        margin-bottom: 10 * $units;  
-        margin-right: 1 * $units; 
-        grid-gap: 1 * $units; 
-        display: flex; 
-        flex-direction: column; 
-        justify-content: center; 
-        align-items: center; 
-
-    }
-
-    .menu {
-
-    ul {
-        margin-top: 20 * $units;
-    } 
-
-    li {
-      
-      padding: 2 * $units;   
-      list-style: none; 
-      width: 100%; 
-      border-left: 0.5 * $units solid var(--invisi);
-
-     &:hover {
-          border-left: 0.5 * $units solid $primary;
-          transition: 0.3s ease;
-        }
-     }
-
-     button {
-         border: none; 
-         background: none; 
-         color: $primary; 
-         font-family: 'Puritan', sans-serif;
-         font-size: 2 * $units; 
-         outline: inherit;
-     }
-
-
-    a {
-      padding: 2 * $units;     
-      color: var(--textcol);  
-      text-decoration: none; 
-      width: 100%;       
-    }
-
-    i {
-        color: $primary; 
-    }
-
-
-    }
-    
-
-  .container {
-      border-radius: 1%;
-      height: 100vh; 
-      width: 100vw; 
-    //   border: 1px solid var(--bordercol); 
-      margin: 5 * $units; 
-      display: grid; 
-      grid-template-columns: auto auto; 
+  .dash-content {
+      grid-area: dashcontent; 
   }
-
-  .dash-content{
-    margin-top: -37 * $units;   
-    margin-left: 120 * $units; 
-    border: 1px solid var(--bordercol); 
-    padding: 20 * $units; 
-    width: 40vw; 
-    height: 60vh; 
-    display: flex; 
-    flex-direction: column; 
-    align-items: center; 
-    justify-content: center; 
-
-    .welcomemsg {
-        font-family: 'Puritan', sans-serif;
-        color: $primary;
-        animation: mymove 5s infinite;
-    }
-
-     img {
-        margin-left: 2.5 * $units; 
-        border-radius: 50%;
-       
-    }
-
-    .logout {
-       
-        margin-left: 0.5 * $units; 
-        margin-top: 2 * $units; 
-    }
-
-    .buttonbar {
-        width: 10vw; 
-    }
-    // .dashdefault, .dashemail, .settingsform {
-    //    display: none; 
-    // }
-
-    }
-
-  
-
 
 
     
